ff7f001265fb6153f006d795286f5e6fb1f02f47
RESQ Chain <PaulH-RESQ>
2019-03-19 12:14:33 +0200

    Windows Build Fix

--- src/qt/walletview.cpp
+++ src/qt/walletview.cpp
@@ -177,13 +177,13 @@ WalletView::WalletView(QWidget* parent) : QStackedWidget(parent),
     QVBoxLayout* vbox_2 = new QVBoxLayout();
     vbox_2->addWidget(frame_Header_2);
 
-    proposalList = new ProposalList(this);
-    vbox_2->addWidget(proposalList);
+    // proposalList = new ProposalList(this);
+    // vbox_2->addWidget(proposalList);
 
     vbox_2->setStretch(1, 1);
 
-    proposalListPage->setLayout(vbox_2);
-    addWidget(proposalListPage);
+    // proposalListPage->setLayout(vbox_2);
+    // addWidget(proposalListPage);
 
     // Clicking on a transaction on the overview pre-selects the transaction on the transaction history page
     connect(overviewPage, SIGNAL(transactionClicked(QModelIndex)), transactionView, SLOT(focusTransaction(QModelIndex)));
